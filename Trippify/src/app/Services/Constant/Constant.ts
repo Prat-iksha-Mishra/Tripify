c93e7ba369c302c24d86c2d45e936825c0af527f
Prat-iksha-Mishra <pratikshamishra054@gmail.com>
2024-05-28 18:27:53 +0530

    Fixed design and made contact page work and query shown below the contct pge

--- Trippify/src/app/Services/Constant/Constant.ts
+++ Trippify/src/app/Services/Constant/Constant.ts
@@ -1,5 +1,7 @@
 export const Constant ={
   API_END_POINT : 'https://localhost:7236/api/Trippify/',
+  API_END_POINT_FOR_CONTACT : 'https://localhost:7236/api/Contact/',
+
   METHODS:{
       CREATE_LOCATION : 'AddLocation',
       CREATE_CATEGORY : 'AddCategory',
@@ -8,7 +10,10 @@ export const Constant ={
       GET_ALL_CATEGORY : 'GetAllCategory',
       GET_ALL_LOCATION : 'GelAllLocation',
       GET_IMAGE : 'GetFile',
-      GET_LOCATION_BY_ID : 'GetLocationById'
+      GET_LOCATION_BY_ID : 'GetLocationById',
+      ADD_CUSTOMER_QUERY : 'AddCustomerQuery',
+      GET_CUSTOMER_QUERY : 'GetAllQuery'
+      
 
   }
 }
